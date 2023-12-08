37aed4c5bf3239c7c31fe38fc19004400b098c39
watany <76135106+watany-dev@users.noreply.github.com>
2023-12-08 08:57:10 +0000

    format

--- packages/open-telemetry/src/trace/instrumentation.ts
+++ packages/open-telemetry/src/trace/instrumentation.ts
@@ -1,30 +1,26 @@
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
-import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { Resource } from '@opentelemetry/resources'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 
 interface TracingOptions {
-  serviceName: string;
-  exporter?: any;
-  additionalAttributes?: Record<string, any>;
+  serviceName: string
+  exporter?: any
+  additionalAttributes?: Record<string, any>
 }
 
 export const initializeTracing = (options: TracingOptions) => {
-  const {
-    serviceName,
-    exporter = new ConsoleSpanExporter(),
-    additionalAttributes = {},
-  } = options;
+  const { serviceName, exporter = new ConsoleSpanExporter(), additionalAttributes = {} } = options
 
   const resource = new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
     ...additionalAttributes,
-  });
+  })
 
   const sdk = new NodeSDK({
     resource: resource,
     traceExporter: exporter,
-  });
+  })
 
-  sdk.start();
-};
+  sdk.start()
+}
--- packages/open-telemetry/src/trace/tracer.ts
+++ packages/open-telemetry/src/trace/tracer.ts
@@ -1,5 +1,5 @@
-import { Context } from "hono";
-import { trace, Span } from "@opentelemetry/api";
+import { Span, trace } from '@opentelemetry/api'
+import { Context } from 'hono'
 import { createMiddleware } from 'hono/factory'
 
 const recordError = (span: Span, error: unknown) => {
@@ -8,44 +8,44 @@ const recordError = (span: Span, error: unknown) => {
       name: error.name,
       message: error.message,
       stack: error.stack,
-    });
-    span.setStatus({ code: 2, message: error.message });
+    })
+    span.setStatus({ code: 2, message: error.message })
   } else {
-    const errorMessage = String(error);
-    span.recordException({ message: errorMessage });
-    span.setStatus({ code: 2, message: errorMessage });
+    const errorMessage = String(error)
+    span.recordException({ message: errorMessage })
+    span.setStatus({ code: 2, message: errorMessage })
   }
-};
+}
 
 export const otelTracer = (
   tracerName: string,
-  customAttributes?: (context: Context) => Record<string, unknown>
+  customAttributes?: (context: Context) => Record<string, unknown>,
 ) => {
   return createMiddleware(async (c: Context, next: () => Promise<void>) => {
-    const tracer = trace.getTracer(tracerName);
-    const span = tracer.startSpan("http-request", {
+    const tracer = trace.getTracer(tracerName)
+    const span = tracer.startSpan('http-request', {
       attributes: {
-        "http.method": c.req.method,
-        "http.url": c.req.url,
+        'http.method': c.req.method,
+        'http.url': c.req.url,
         ...customAttributes,
       },
-    });
-    const startTime = Date.now();
+    })
+    const startTime = Date.now()
 
     try {
-      await next();
-      span.setAttribute("http.status_code", c.res.status);
+      await next()
+      span.setAttribute('http.status_code', c.res.status)
     } catch (error) {
-      recordError(span, error);
-      if (c.req.header("User-Agent")) {
-        const userAgent = c.req.header("User-Agent") || "Unknown";
-        span.setAttribute("http.user_agent", userAgent);
+      recordError(span, error)
+      if (c.req.header('User-Agent')) {
+        const userAgent = c.req.header('User-Agent') || 'Unknown'
+        span.setAttribute('http.user_agent', userAgent)
       }
-      throw error;
+      throw error
     } finally {
-      const duration = Date.now() - startTime;
-      span.setAttribute("http.request_duration", duration);
-      span.end();
+      const duration = Date.now() - startTime
+      span.setAttribute('http.request_duration', duration)
+      span.end()
     }
   })
-};
+}
